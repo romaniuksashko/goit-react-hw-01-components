f8f2b362139f07d2e6143655a5e6917a01138103
Oleksandr Romaniuk <romanuksashko@gmail.com>
2026-04-21 09:56:10 +0300

    status

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -3,7 +3,7 @@ import style from "./FriendListItem.module.css";
 function FriendListItem({ friends }) {
   return (friends.map(({avatar, name, isOnline, id}) => {
     return (<li className={style.item} key={id}>
-      <span className={style.status}>{isOnline}</span>
+      <span className={isOnline ? style.online : style.offline}>{isOnline}</span>
       <img className={style.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={style.name}>{name}</p>
     </li>);
